69129439f955f15375a352cc9ac4ee5dcbd8affc
贺登耀 <hedengyao@lexianyi.com>
2016-12-30 18:14:39 +0800

    1
    1

---
+++ src/pages/session-detail/session-detail.ts
@@ -0,0 +1,17 @@
+
+import { Component } from '@angular/core';
+
+import { NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-session-detail',
+  templateUrl: 'session-detail.html'
+})
+export class SessionDetailPage {
+  session: any;
+
+  constructor(public navParams: NavParams) {
+    this.session = navParams.data;
+  }
+}
